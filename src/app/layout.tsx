d44d38f4f12a7557d3052f1ecaf87f2010edc42c
Acharya Group <acharyasumit786@gmail.com>
2025-10-04 14:58:35 +0530

    change talk to content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,27 +22,24 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       </head>
       <body>
         <ClientProvider>{children}</ClientProvider>
-          <Script id="tawk-to" strategy="afterInteractive">
-  {`
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function(){
-      var s1 = document.createElement("script"),
-          s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = 'https://embed.tawk.to/5b2b664cd0b5a54796820793/1j4cbl1fi';
-      s1.charset = 'UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1, s0);
-    })();
-
-    // 👇 Force a custom visitor name
-    Tawk_API.onLoad = function() {
-      Tawk_API.setAttributes({
-        'name': 'Acharya Assistant'   
-      }, function(error){});
-    };
-  `}
-</Script>
+           <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+              (function(){
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/5b2b664cd0b5a54796820793/1j6n68n77';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
