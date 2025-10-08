3880e4913b16df75898cb14091f851cf046b1a42
Acharya Group <acharyasumit786@gmail.com>
2025-10-08 15:49:53 +0530

    popup fix

--- src/componets/sections/Popup.tsx
+++ src/componets/sections/Popup.tsx
@@ -31,48 +31,57 @@ const Popup: React.FC = () => {
     >
       <div className="relative">
            {/* Close Button */}
-        <div onClick={() => setVisible(false)} className="h-8 absolute hover:bg-secondary top-4 right-4  cursor-pointer flex justify-center items-center z-10 w-8 bg-primary rounded-full">
+        <div onClick={() => setVisible(false)} className="h-5 absolute bg-primary hover:bg-secondary top-4 right-4  cursor-pointer flex justify-center items-center z-10 w-5 rounded-full">
           <button
             
-            className=" text-white cursor-pointer font-bold text-2xl"
+            className=" text-primary cursor-pointer text-white font-bold text-base"
           >
             ✕
           </button>
         </div>
-        <div
-          className="bg-white rounded-xl shadow-lg max-w-[220px] md:w-full sm:max-w-md h-[400px] md:h-[600px] relative overflow-auto touch-auto"
-          onClick={(e) => e.stopPropagation()}
-          style={{ WebkitOverflowScrolling: "touch" }}
-        >
-        
-          {/* Swiper Slider */}
-          <Swiper
-            modules={[Autoplay]}
-            autoplay={{ delay: 3000, disableOnInteraction: false }}
-            loop={true}
-            slidesPerView={1}
+      <div
+  className="bg-white rounded-xl shadow-lg max-w-[220px] md:w-full sm:max-w-md max-h-[500px] relative overflow-auto"
+  onClick={(e) => e.stopPropagation()}
+  style={{ WebkitOverflowScrolling: "touch" }}
+>
+  {/* Swiper Slider */}
+  <Swiper
+    modules={[Autoplay]}
+    autoplay={{ delay: 3000, disableOnInteraction: false }}
+    loop={true}
+    slidesPerView={1}
+  >
+    {allPopups.data.map((popup: PopupType) => (
+      <SwiperSlide key={popup._id}>
+        <a href={popup.link} target="_blank" rel="noopener noreferrer">
+          <div
+            className="w-full max-h-[500px] overflow-auto"
+            onTouchStart={(e) => e.stopPropagation()}
+            style={{ WebkitOverflowScrolling: "touch" }}
           >
-            {allPopups.data.map((popup: PopupType) => (
-              <SwiperSlide key={popup._id}>
-                <a href={popup.link} target="_blank" rel="noopener noreferrer">
-                  <div
-                    className="w-full overflow-auto"
-                    onTouchStart={(e) => e.stopPropagation()}
-                    style={{ WebkitOverflowScrolling: "touch" }}
-                  >
-                    <Image
-                      src={popup.image}
-                      alt="Popup Image"
-                      width={400}
-                      height={800}
-                      className="w-full object-contain"
-                    />
-                  </div>
-                </a>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+            <Image
+              src={popup.image}
+              alt="Popup Image"
+              width={400}
+              height={800}
+              className="w-full h-auto object-contain"
+            />
+          </div>
+        </a>
+      </SwiperSlide>
+    ))}
+  </Swiper>
+</div>
+
+        <div onClick={() => setVisible(false)} className="px-2 py-1 absolute hover:bg-secondary bottom-4 right-4  cursor-pointer flex justify-center items-center z-10  bg-primary rounded-full">
+          <button
+            
+            className=" text-white cursor-pointer  text-xs"
+          >
+            close
+          </button>
         </div>
+       
       </div>
     </div>
   );
